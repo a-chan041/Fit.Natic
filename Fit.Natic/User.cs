f4c9a715d60c585c776982e185d9755ba2f4daee
Michael McDermott <michaelmcdermott@Michaels-MacBook-Pro.local>
2020-04-01 14:27:08 -0700

    added unit test project to solution, but there arent any tests coded yet.

--- Fit.Natic/User.cs
+++ Fit.Natic/User.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 namespace Fit.Natic
 {
+    /* User class is instantiated when app is loaded,
+     * variables are read in from json, and when edited, saved to json
+     * 
+     */
+
+//TODO: create a function that reads in json to create the user object,
+        //function can either live here or in app.xaml.cs
+
     public class User
     {
         public string name { get; set; }
@@ -15,6 +23,19 @@ namespace Fit.Natic
         {
 
         }
+
+        public readFromJson()
+        {
+
+        }
+
+        /* Would be used by front end if the user makes any changes to their daily target
+         * Should take in a value for each of the daily target values and update all of them.
+         */
+        public Boolean updateDailyTarget() {
+            
+        }
+
 
         public string getDailyTarget()
         {
